5d5eeb06353040582e8272614e4d512a64dfcae4
Vladimir <vovashadrov@yandex.ru>
2021-03-29 01:16:55 +0300

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+import '../styles/main.scss';
+import Layout from '../components/Layout.jsx';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
